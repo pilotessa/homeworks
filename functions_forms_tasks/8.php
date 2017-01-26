74dfe458318148b5ce5e0bfc2d71058cdfeb568e
pilotessa <pilotessa@gmail.com>
2017-01-26 18:05:46 +0200

    Change formatting

--- functions_forms_tasks/8.php
+++ functions_forms_tasks/8.php
@@ -27,7 +27,7 @@ function checkForbiddenWords($string)
     $parts = preg_split($delimiters, $string, -1, PREG_SPLIT_NO_EMPTY);
     // Переводим слова в нижний регистр
     $parts = array_map('mb_strtolower', $parts);
-    
+
     // Есть совпадения с запрещенными словами?
     $result = array_intersect($parts, getForbiddenWords());
     return !count($result);
