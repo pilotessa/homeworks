73adaee386980f481af03920acba49325452e65e
pilotessa <pilotessa@gmail.com>
2017-02-12 10:22:30 +0200

    Fix lang

--- arrays_loops_tasks/23.php
+++ arrays_loops_tasks/23.php
@@ -17,17 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>23</title>
-</head>
-<body>
-<?=isset($result) ? $result : ''?>
+<?= isset($result) ? $result : '' ?>
 <form action="" method="post">
     <p>
         <label for="number">Введите число</label><br>
@@ -37,5 +27,3 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <button>Вперед!</button>
     </p>
 </form>
-</body>
-</html>
--- php_basics_tasks/show.php
+++ php_basics_tasks/show.php
@@ -9,7 +9,7 @@ if (file_exists($filename)) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
